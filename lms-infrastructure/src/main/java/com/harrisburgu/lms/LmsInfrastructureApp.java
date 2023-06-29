930d599ee07c963f21793843272bed9ff3b9d280
Joseph <fisayoayodele01@gmail.com>
2023-06-28 23:53:32 -0500

    added ec2, rds, and s3 infrastructure

--- lms-infrastructure/src/main/java/com/harrisburgu/lms/LmsInfrastructureApp.java
+++ lms-infrastructure/src/main/java/com/harrisburgu/lms/LmsInfrastructureApp.java
@@ -1,6 +1,7 @@
 package com.harrisburgu.lms;
 
 import software.amazon.awscdk.App;
+import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
 import java.util.Map;
@@ -8,14 +9,24 @@ import java.util.Map;
 public class LmsInfrastructureApp {
     private static final String LmsInfrastructureStack = "LmsInfrastructureStack";
     
+    static Environment makeEnv(String account, String region) {
+        account = (account == null) ? System.getenv("CDK_DEFAULT_ACCOUNT") : account;
+        region = (region == null) ? System.getenv("CDK_DEFAULT_REGION") : region;
+
+        return Environment.builder()
+                .account(account)
+                .region(region)
+                .build();
+    }
+    
     public static void main(final String[] args) {
         App app = new App();
-        System.out.println(System.getenv("CDK_DEFAULT_ACCOUNT"));
 
         new LmsInfrastructureStack(app, LmsInfrastructureStack, 
                 StackProps.builder()
                         .description("Library Management System Stack")
                         .tags(Map.of("env","lms"))
+                        .env(makeEnv(null, null))
                         .build());
 
         app.synth();
--- lms-infrastructure/src/main/java/com/harrisburgu/lms/LmsInfrastructureStack.java
+++ lms-infrastructure/src/main/java/com/harrisburgu/lms/LmsInfrastructureStack.java
@@ -1,54 +1,92 @@
 package com.harrisburgu.lms;
 
 import software.amazon.awscdk.RemovalPolicy;
-import software.amazon.awscdk.SecretValue;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.ec2.AmazonLinuxEdition;
+import software.amazon.awscdk.services.ec2.AmazonLinuxGeneration;
+import software.amazon.awscdk.services.ec2.AmazonLinuxImage;
+import software.amazon.awscdk.services.ec2.AmazonLinuxStorage;
+import software.amazon.awscdk.services.ec2.AmazonLinuxVirt;
+import software.amazon.awscdk.services.ec2.IMachineImage;
+import software.amazon.awscdk.services.ec2.IVpc;
 import software.amazon.awscdk.services.ec2.Instance;
 import software.amazon.awscdk.services.ec2.InstanceClass;
 import software.amazon.awscdk.services.ec2.InstanceSize;
-import software.amazon.awscdk.services.ec2.MachineImage;
+import software.amazon.awscdk.services.ec2.InstanceType;
+import software.amazon.awscdk.services.ec2.Peer;
+import software.amazon.awscdk.services.ec2.Port;
+import software.amazon.awscdk.services.ec2.SecurityGroup;
 import software.amazon.awscdk.services.ec2.SubnetSelection;
 import software.amazon.awscdk.services.ec2.SubnetType;
+import software.amazon.awscdk.services.ec2.UserData;
+import software.amazon.awscdk.services.ec2.Vpc;
+import software.amazon.awscdk.services.ec2.VpcLookupOptions;
 import software.amazon.awscdk.services.rds.DatabaseInstance;
 import software.amazon.awscdk.services.rds.DatabaseInstanceEngine;
-import software.amazon.awscdk.services.ec2.InstanceType;
-import software.amazon.awscdk.services.rds.IInstanceEngine;
-import software.amazon.awscdk.services.rds.MariaDbInstanceEngineProps;
+import software.amazon.awscdk.services.rds.StorageType;
+import software.amazon.awscdk.services.s3.Bucket;
 import software.constructs.Construct;
-import software.amazon.awscdk.Stack;
-import software.amazon.awscdk.StackProps;
 
 public class LmsInfrastructureStack extends Stack {
-    public LmsInfrastructureStack(final Construct scope, final String id) {
-        this(scope, id, null);
-    }
 
     public LmsInfrastructureStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        final IVpc vpc = Vpc.fromLookup(this, "DefaultVPC", VpcLookupOptions.builder().isDefault(true).build());
+        
         // create rds
         // return endpoint
-        final IInstanceEngine instanceEngine = DatabaseInstanceEngine.mariaDb(
-                MariaDbInstanceEngineProps.builder().build()
-        );
-
-        final DatabaseInstance databaseInstance = DatabaseInstance.Builder.create(this, id + "-rds")
+        final DatabaseInstance instance = DatabaseInstance.Builder.create(this, id + "-rds")
                 .instanceType(InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO))
-                .engine(instanceEngine)
-                .instanceIdentifier(id + "-rds")
+                .engine(DatabaseInstanceEngine.MARIADB)
+                .storageType(StorageType.GP2)
                 .allocatedStorage(20)
+                .vpc(vpc)
+                .publiclyAccessible(true)
+                .vpcSubnets(SubnetSelection.builder().subnetType(SubnetType.PUBLIC).build())
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .build();
-        
+
+        // ec2 instance security group
+        SecurityGroup securityGroup = SecurityGroup.Builder.create(this, id + "sg")
+                .vpc(vpc)
+                .description("Allow ssh access to ec2 instances")
+                .allowAllOutbound(true)
+                .disableInlineRules(true)
+                .build();
+        securityGroup.addIngressRule(Peer.anyIpv4(), Port.tcp(22), "allow ssh access from the world");
+
+        // ec2 instance image
+        IMachineImage image = AmazonLinuxImage.Builder.create()
+                .generation(AmazonLinuxGeneration.AMAZON_LINUX)
+                .edition(AmazonLinuxEdition.STANDARD)
+                .virtualization(AmazonLinuxVirt.HVM)
+                .storage(AmazonLinuxStorage.GENERAL_PURPOSE)
+                .build();
+
+        UserData userData = UserData.forLinux();
+        userData.addCommands("""
+                touch hello.txt
+                """);
+
         // create ec2 instance
         // use scripts to install java
         // environment variable for rds endpoint
         // Create an EC2 instance
-        Instance ec2Instance = Instance.Builder.create(this, "Ec2Instance")
+        Instance ec2Instance = Instance.Builder.create(this, id + "-ec2")
                 .instanceType(InstanceType.of(InstanceClass.BURSTABLE2, InstanceSize.MICRO))
+                .machineImage(image)
+                .vpc(vpc)
+                .securityGroup(securityGroup)
+                .userData(userData)
+                .keyName("lms-kp")
                 .build();
 
         // create s3 bucket for user interface
         // variable for backend endpoint
-
+        Bucket websiteBucket = Bucket.Builder.create(this, id + "-s3")
+                .removalPolicy(RemovalPolicy.DESTROY)
+                .build();
     }
 }
